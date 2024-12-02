f9b7414113646ef35be6a7d4277abf92ab634996
Ian Contijoch <ian.contijoch@gmail.com>
2024-12-02 07:08:02 -0500

    day02-part1

---
+++ day02/part1.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    safe = 0 
+    lines = s.splitlines()
+    for line in lines:
+        report = list(map(int, line.split()))
+        if (
+            all((a < b) and (b - a  >= 1 and b - a <= 3) 
+                for a, b in zip(report, report[1:]))
+            or
+            all((a > b) and (a - b  >= 1 and a - b <= 3) 
+                for a, b in zip(report, report[1:]))
+        ):
+            safe += 1
+            
+        
+    return safe
+
+
+INPUT_S = '''\
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+'''
+EXPECTED = 2
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
