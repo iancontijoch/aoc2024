b38d1fa7ffc58c997ca0918eb3d6f805125cfdd9
Ian Contijoch <ian.contijoch@gmail.com>
2024-12-03 18:36:34 -0500

    day03-part1

---
+++ day03/part1.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+import re
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    total = 0
+    for line in lines:
+        total += sum(int(a) * int(b) for a, b in re.findall(r'mul\((\d+),(\d+)\)', line))
+    return total
+
+
+INPUT_S = '''\
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+'''
+EXPECTED = 161
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
