5e97647f6a8b47fe70687ed22cf7113ae82d7036
Ian Contijoch <ian.contijoch@gmail.com>
2024-12-06 07:58:38 -0500

    day05-part1

---
+++ day05/part1.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    total = 0
+    flag = True
+    ordering, updates = s.split('\n\n')
+    lst = []
+
+    for update in updates.splitlines():
+        lst = list(map(int, update.split(',')))
+
+        for order in ordering.splitlines():
+            first, second = map(int, order.split('|'))
+            if first in lst and second in lst:
+                if lst.index(first) >= lst.index(second):
+                    flag = False
+        if flag:
+            total += lst[len(lst) // 2]
+        flag = True
+    return total
+
+
+INPUT_S = '''\
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+'''
+EXPECTED = 143
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
